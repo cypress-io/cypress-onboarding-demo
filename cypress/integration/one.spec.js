56f9c1114d05a607eaceaa9b61ff9e89c7120d7b
Brian Mann <brian.mann86@gmail.com>
2021-06-21 16:46:39 -0400

    add t4, modify t2

--- cypress/integration/one.spec.js
+++ cypress/integration/one.spec.js
@@ -8,11 +8,16 @@ it('t1', () => {
 })
 
 it('t2', () => {
-  cy.log('t2')
+  cy.log('t2 v2')
   cy.wait(2000)
 })
 
 it('t3', () => {
   cy.log('t3')
+  cy.wait(2000)
+})
+
+it('t4', () => {
+  cy.log('t4')
   cy.wait(2000)
 })
